0a4bd659612d7a056ebe05d0a4272b04b1bc71ac
Dilmurod <mr.j0k3r.d1ma@gmail.com>
2025-08-23 15:17:28 +0500

    computer reinstalled

--- frontend/config/main.php
+++ frontend/config/main.php
@@ -21,6 +21,8 @@ return [
         'gridview' => [
             'class' => '\kartik\grid\Module',
         ],
+
+
         'service' => [
             'class' => 'frontend\modules\service\Module',
         ],
